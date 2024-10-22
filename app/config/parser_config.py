64cf3dad040a3b400fe7f443ca835b1759fa9caa
carlos ochoa <carlosoaly@gmail.com>
2024-10-22 17:25:39 -0500

    Avance parse

--- app/config/parser_config.py
+++ app/config/parser_config.py
@@ -2,10 +2,9 @@ import argparse
 def getParser():
     parser = argparse.ArgumentParser(prog='Session Manager')
     parser.add_argument('--check',action='store_true', help='Revisa la configuracion de los servicios')
-    parser.add_argument('--delay', nargs='?', default=5, choices=range(5, 21),
-                        help='Definir el intervalo en seguntos entre cada ejecucion del servicio durante 1 minuto',
-                        required=True,
-                        type=int)
     parser.add_argument('-D', '--daemon', action='store_true', help='Activar ejecucion en segundo plano')
+    parser.add_argument('-i','--interval', nargs='?', default=5, choices=range(5, 21),
+                        help='Definir el intervalo en seguntos entre cada ejecucion',
+                        type=int)
     #__create_base_parser(parser)
     return parser
--- app/main.py
+++ app/main.py
@@ -3,24 +3,28 @@ import config.parser_config as parser_config
 import signal
 from config.container import Container
 
-
 logger = log_config.getLogger()
 container = Container()
+
+
 def main():
     container.config.from_yaml("./credentials.yml")
     container.wire(modules=[__name__])
     parser = parser_config.getParser()
     args = parser.parse_args()
     logger.info(args)
-    if args.check and args.command is None:
+    if args.check:
         logger.info(f"Revision de configuracion")
-
+    elif args.daemon:
+        logger.info(f"Iniciando modo daemon")
     else:
         logger.error(f"Comando desconocido")
 
 def handle_sigterm(signum, frame):
     logger.info(f"Se recibió la señal SIGTERM. Deteniendo el servicio...")
     exit(0)
+
+
 if __name__ == "__main__":
     main()
     signal.signal(signal.SIGTERM, handle_sigterm)
